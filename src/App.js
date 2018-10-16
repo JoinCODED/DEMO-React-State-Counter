5fa059cc5d4221702dd082e33aaf9cd970aa9319
Octowl <alsaff1987@gmail.com>
2018-10-16 17:53:22 +0300

    Update to include binding and proper setState usage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <button className="btn btn-success">+</button>
-      <p className="inline">0</p>
-      <button className="btn btn-danger">-</button>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { number: 0 };
+
+    this.increase = this.increase.bind(this);
+    this.decrease = this.decrease.bind(this);
+  }
+
+  increase() {
+    const newNumber = this.state.number + 1;
+    this.setState({ number: newNumber });
+  }
+
+  decrease() {
+    const newNumber = this.state.number - 1;
+    this.setState({ number: newNumber });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.increase} className="btn btn-success">
+          +
+        </button>
+        <p className="inline">{this.state.number}</p>
+        <button onClick={this.decrease} className="btn btn-danger">
+          -
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
