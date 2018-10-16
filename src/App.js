8c822416fe38dffcbd3f31300d3c64d58299866e
Octowl <alsaff1987@gmail.com>
2018-10-16 17:28:57 +0300

    Code styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-
   render() {
     return (
-      <div className="App" >
-      <button className="btn btn-success">+</button>
+      <div className="App">
+        <button className="btn btn-success">+</button>
         <p className="inline">0</p>
-      <button className="btn btn-danger">-</button>
-        
+        <button className="btn btn-danger">-</button>
       </div>
     );
   }
