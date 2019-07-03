11fddb636bc0e85d35e0854dc996808007c7efb7
Lailz <lailaabdulraheem@gmail.com>
2019-07-03 14:27:57 +0300

    updated demo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <button className="btn btn-success">+</button>
-        <p className="inline">0</p>
-        <button className="btn btn-danger">-</button>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-success">+</button>
+      <p className="inline">0</p>
+      <button className="btn btn-danger">-</button>
+    </div>
+  );
 }
 
 export default App;
